e43ef372ee2fc2e194c8d3d3eefad063af606f65
Minh Quan Do <mdo6180@gmail.com>
2023-05-23 01:42:38 -0400

    flask approach works

--- executor/main.py
+++ executor/main.py
@@ -1,21 +1,15 @@
-import socket
+import requests
 
+ip_address = "192.168.0.172"
+port = "12345"
+end_point = "receive-command"
+external_server_url = f'http://{ip_address}:{port}/{end_point}'  
 
-HOST = '192.168.0.172'  # Replace with the server's IP address or domain name
-PORT = 12345                  # Replace with the port the server is listening on
+command = 'Do something'
 
+response = requests.post(external_server_url, data=command)
 
-if __name__ == "__main__":
-
-    # Establish a connection to the server
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((HOST, PORT))
-
-        # Send a command to the server
-        command = "Do something"
-        client_socket.sendall(command.encode())
-
-        # Receive a response (optional)
-        response = client_socket.recv(1024).decode()
-        print("Server response:", response)
-        print("hello")
+if response.status_code == 200:
+    print('Command sent successfully')
+else:
+    print('Failed to send command')
--- local_code/main.py
+++ local_code/main.py
@@ -1,36 +1,15 @@
-import socket
+from flask import Flask, request
 
-HOST = '0.0.0.0'  # The server's IP address (use 0.0.0.0 to listen on all available interfaces)
-PORT = 12345     # The port to listen on
+app = Flask(__name__)
 
-def process_command(command):
-    # Process the received command as needed
+@app.route('/receive-command', methods=['POST'])
+def receive_command():
+    command = request.get_data().decode()
+
+    # Process the received command
     # Add your logic here
-    print("Received command:", command)
 
-# Create a socket object
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    # Bind the socket to a specific host and port
-    server_socket.bind((HOST, PORT))
+    return 'Command received successfully'
 
-    # Listen for incoming connections
-    server_socket.listen()
-
-    print(f"Server listening on {HOST}:{PORT}")
-
-    # Accept incoming connections
-    while True:
-        client_socket, client_address = server_socket.accept()
-
-        # Receive data from the client
-        command = client_socket.recv(1024).decode()
-
-        # Process the received command
-        process_command(command)
-
-        # Send a response (optional)
-        response = "Command received successfully"
-        client_socket.sendall(response.encode())
-
-        # Close the client connection
-        client_socket.close()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=12345)
