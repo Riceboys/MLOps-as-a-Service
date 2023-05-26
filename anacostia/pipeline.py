68e142926b1fbe09cf42b85e0de14582b5914274
Minh Quan Do <mdo6180@gmail.com>
2023-05-26 02:10:15 -0400

    can now push and pull images from docker hub using dagger.io

--- anacostia/pipeline.py
+++ anacostia/pipeline.py
@@ -2,28 +2,41 @@ import sys
 import anyio
 import dagger
 from dagger.api.gen import BuildArg
+from dotenv import load_dotenv
+import os
 
 
 async def main():
+
+    load_dotenv("./.env")
+    DOCKER_HUB_USERNAME = os.getenv("DOCKER-HUB-USERNAME")
+    DOCKER_HUB_PASSWORD = os.getenv("DOCKER-HUB-PASSWORD")
+
     config = dagger.Config(log_output=sys.stdout)
 
     # initialize Dagger client
     async with dagger.Connection(config) as client:
+
+
+        # set secret as string value
+        secret = client.set_secret("password", DOCKER_HUB_PASSWORD)
+
         # set build context
         context_dir = client.host().directory("./mlflow")
 
-        """
         # build using Dockerfile in ./mlflow directory
-        image_ref = (
-            await context_dir.docker_build()
-            .publish("docker.io/anacostia/mlflow:0.1.0")
+        image_ref = await context_dir.docker_build()
+        
+        mlflow_container_name = await (
+            image_ref
+            .with_registry_auth("docker.io", DOCKER_HUB_USERNAME, secret)
+            .publish(f"{DOCKER_HUB_USERNAME}/anacostia:latest")
         )
-        """
 
         mlflow_container = (
             client.container()
-            .from_("docker.io/mlflow-image:latest")
-            .with_exec(["python", "-V"])
+            .from_(f"{DOCKER_HUB_USERNAME}/anacostia:latest")
+            .with_exec(["mlflow", "--version"])
         )
 
         version = await mlflow_container.stdout()
@@ -31,21 +44,6 @@ async def main():
         # print output
         print(f"Hello from Dagger and mlflow version {version}")
 
-        """
-        # use a python:3.11-slim container
-        # get version
-        python = (
-            client.container()
-            .from_("python:3.11-slim")
-            .with_exec(["python", "-V"])
-        )
-
-        # execute
-        version = await python.stdout()
-
-    # print output
-    print(f"Hello from Dagger and {version}")
-        """
 
 if __name__ == "__main__":
     anyio.run(main)
