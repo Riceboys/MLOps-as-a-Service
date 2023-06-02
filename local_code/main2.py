013cd93440df4497968c307ba7e96db942e3768c
Minh Quan Do <mdo6180@gmail.com>
2023-06-01 23:51:24 -0400

    committing for now before editing AnacostiaSocket class

--- local_code/main2.py
+++ local_code/main2.py
@@ -1,11 +1,72 @@
-from flask import Flask, request
+import socket
+import os
 
-app = Flask(__name__)
+class AnacostiaSocket:
+    def __init__(self) -> None:
+        self.connection = None
+        self.functions = []
+    
+        self.socket_path = "/tmp/anacostia_socket.sock"
+        if os.path.exists(self.socket_path) == True:
+            os.remove(self.socket_path)
+        print("Server listening on", self.socket_path)
 
-@app.route("/prepare-data", methods=["POST"])
-def func():
-    print("it's working.")
-    return "works"
+    def train_handler(self, expected_message):
+        def decorator(func):
+            def wrapper():
+                data = self.connection.recv(1024)
+                if data.decode() == expected_message:
+                    response = func()
+                self.connection.sendall(response.encode())
+                self.connection.close()
+            return wrapper
+        return decorator
+    
+
+ana_sock = AnacostiaSocket()
+
+@ana_sock.train_handler("train1")
+def start_processing():
+    print("Processing started")
+    return "signal received"
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", 8080)
+    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind(ana_sock.socket_path)
+        server_socket.listen(1)
+
+        while True:
+            connection, _ = server_socket.accept()
+            start_processing()
+
+"""
+def message_handler(expected_message):
+    def decorator(func):
+        def wrapper(connection):
+            data = connection.recv(1024)
+            if data.decode() == expected_message:
+                response = func()
+            connection.sendall(response.encode())
+            connection.close()
+        return wrapper
+    return decorator
+
+@message_handler("start")
+def start_processing():
+    print("Processing started")
+    return "signal received"
+
+socket_path = "/tmp/anacostia_socket.sock"
+if os.path.exists(socket_path) == True:
+    os.remove(socket_path)
+
+server_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+server_socket.bind(socket_path)
+server_socket.listen(1)
+
+print("Server listening on", socket_path)
+
+while True:
+    connection, _ = server_socket.accept()
+    start_processing(connection)
+"""
---
+++ local_code/main3.py
@@ -0,0 +1,15 @@
+import socket
+
+# Connect to the server using the same socket path
+socket_path = "/tmp/anacostia_socket.sock"
+
+client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+client_socket.connect(socket_path)
+
+message = "train1"
+client_socket.sendall(message.encode())
+
+response = client_socket.recv(1024)
+print("Server response:", response.decode())
+
+client_socket.close()
