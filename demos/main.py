0ffc6e4d949344c54764e3a463a31a57759a8079
Minh Quan Do <mdo6180@gmail.com>
2023-06-10 20:15:58 -0400

    started development of conditional triggers

--- demos/main.py
+++ demos/main.py
@@ -1,11 +1,15 @@
 import sys
 sys.path.append("../anacostia-client")
+import os
 
-from api import MLOpsPipeline
+from api import AnacostiaPipeline
+from client import AnacostiaExecutor
 import schedule
 
+from typing import Callable
 
-pipeline = MLOpsPipeline()
+
+pipeline = AnacostiaPipeline()
 
 @pipeline.train("training started", "trainind ended", "OKBLUE")
 def train_model():
@@ -19,5 +23,10 @@ def prepare_data():
 
 
 if __name__ == "__main__":
-    cron_expr = "0/5 0 0 ? * * *"
+    #AnacostiaExecutor(host_inbound_port=8000, host_outbound_port=12345)
     pipeline.run()
+
+    #cron_string = "* * * * *"
+
+    #trigger_training(count_files_in_dir, "./data", 0)
+
