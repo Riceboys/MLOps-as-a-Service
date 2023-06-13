e16bfbbdf6f420073994c36e2d53a90e79c424a8
Minh Quan Do <mdo6180@gmail.com>
2023-06-13 16:32:26 -0400

    trigger can now be ran conditionally and on schedule

--- anacostia_client/trigger.py
+++ anacostia_client/trigger.py
@@ -5,42 +5,53 @@ from anacostia_client import scheduler, lock
 
 from apscheduler.triggers.base import BaseTrigger
 from apscheduler.triggers.interval import IntervalTrigger
+from collections.abc import Callable
+
 
 class AnacostiaTrigger:
     def __init__(
             self, 
             trigger_name: str,  
             stages: list[str] | str = "all",
-            trigger_schedule: BaseTrigger = IntervalTrigger(seconds=1)
+            trigger_schedule: BaseTrigger = IntervalTrigger(seconds=1),
+            trigger_function: Callable[..., bool] = None
         ) -> None:
         
         self.trigger_name = trigger_name
         self.trigger_schedule = trigger_schedule
         self.trigger_result = False
+        self.trigger_function = trigger_function
         scheduler.add_job(self.wrapper, self.trigger_schedule, id=self.trigger_name)
-
-    def trigger_function(self):
-        return True
+        print(f"Trigger {self.trigger_name} is running")
     
     def wrapper(self):
         with lock:
-            self.trigger_result = self.trigger_function()
+            if self.trigger_function is not None:
+                self.trigger_result = self.trigger_function()
+            else:
+                self.trigger_result = True
 
     def run(self):
+        scheduler.start()
+        print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
         try:
             while True:
                 with lock:
                     if self.trigger_result:
+                        # replace the following print statement with function to trigger endpoint
                         print(f"{self.trigger_name} triggered")
                         self.trigger_result = False
+
         except (KeyboardInterrupt, SystemExit):
             # Not strictly necessary if daemonic mode is enabled but should be done if possible
             scheduler.shutdown()
+            print(f"\nTrigger {self.trigger_name} stopped")
 
 
 import random
 
-def tick():
+def trigger_func() -> bool:
     num = random.randint(1, 10)
     if num <= 5:
         print(f"random number <= 5: {num}")
@@ -49,30 +60,10 @@ def tick():
         print(f"random number > 5: {num}")
         return False
 
-# once wrapper is put inside of a class, val is no longer global
-val = False
-def wrapper():
-    global val
-    with lock:
-        val = tick()
-
 if __name__ == '__main__':
-    job1 = scheduler.add_job(wrapper, trigger=IntervalTrigger(seconds=3), id='job1')
-    print(f"job_id: {job1.id}")
-
-    # call scheduler.start() after all the triggers have been added
-    scheduler.start()
-    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
-
-    try:
-        # This is here to simulate application activity (which keeps the main thread alive).
-        while True:
-            with lock:
-                if val:
-                    print("val is true")
-                    val = False
-            #time.sleep(2)
-
-    except (KeyboardInterrupt, SystemExit):
-        # Not strictly necessary if daemonic mode is enabled but should be done if possible
-        scheduler.shutdown()
+    train_trigger = AnacostiaTrigger(
+        "train_trigger", 
+        trigger_schedule=IntervalTrigger(seconds=3), 
+        trigger_function=trigger_func
+    )
+    train_trigger.run()
